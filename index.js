7b322c39dc0fd7eaac1b6f72ff932f43ea0b5dc3
xlan11 <alankelly@gmail.com>
2022-07-10 22:07:40 +0100

    fixed second password generating

--- index.js
+++ index.js
@@ -9,19 +9,8 @@ let leftPassword = document.getElementById("password-one")
 let rightPassword = document.getElementById("password-two")
 let generateBtn = document.getElementById("generate-password")
 
+
 generateBtn.addEventListener("click", generatePassword)
-
-// function generatePassword(length) {
-//     let result = ' ';
-//     const charactersLength = characters.length;
-//     for ( let i = 0; i < length; i++ ) {
-//         result += characters.charAt(Math.floor(Math.random() * charactersLength));
-//     }
-
-//     return result;
-// }
-
-// console.log(generatePassword(15));
 
 
 function generatePassword() {
@@ -31,12 +20,13 @@ function generatePassword() {
     
     for (let i = 0; i < pass_length; i++) {
         let rnum = Math.floor(Math.random() * characters.length)
+        let rnum2 = Math.floor(Math.random() * characters.length)
         passwordOne += characters[rnum]
-        passwordTwo += characters[rnum]
+        passwordTwo += characters[rnum2]
     }
 
     leftPassword.innerText=passwordOne
     rightPassword.innerText=passwordTwo
-
     
 }
+
